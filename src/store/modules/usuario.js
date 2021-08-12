d21fed8ac0609678b144aaab43d4b987d082abbf
Erick2418 <erickjoellimon@outlook.com>
2021-08-11 19:01:55 -0500

    btn pushme, aun falta pintarlo en la tabla

--- src/store/modules/usuario.js
+++ src/store/modules/usuario.js
@@ -1,4 +1,5 @@
-
+import axios from 'axios'
+import Swal from 'sweetalert2'
 
 const personas_generadas = [
   {
@@ -43,7 +44,29 @@ export default {
 
       },
       actions: {
+        getUserApi: async function ({commit}) {
+          let apiUsuario;
+          try {
+             apiUsuario = await axios.get('http://localhost:3000/api/user',{
+               headers: {
+                 'x-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEiLCJlbWFpbCI6IklzYWJlbEBnbWFpbC5jb20iLCJpYXQiOjE2Mjg3MjI1OTQsImV4cCI6MTYyODcyOTc5NH0.JZJtNaoFWdTf6g0dAMyxDOZJcsJ_N4Y48QCGBwbZNpU'
+               }
+            }).then(({data}) => {
+              console.log(data);
+              commit('setPersonas',data);
+            });
+            //  this.usersApi=apiUsuario.data.users;
+           }catch (err) {
+             apiUsuario = err.response;
+           
+             Swal.fire({
+               icon: 'error',
+               title: 'Oops...',
+               text: apiUsuario.data.msg + ", inicie sesion e intente nuevamente",
 
+             })
+           }        
+        },
       },
       modules: {
     
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -18,68 +18,45 @@
       :search="search"
     ></v-data-table>
   </v-card>
+  
   <div>
-    {{usersApi}}
+    <button @click="setUserTable" >PUSH ME</button>
+    {{personas}}
   </div>
 </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
- import axios from 'axios'
- import Swal from 'sweetalert2'
+import { mapActions, mapState } from 'vuex'
+
 export default {
     data () {
       return {
-        usersApi:[],
         search: '',
-        // usuario_modelo: { id: 1,name: 'Mishell', email: 'Mishell@gmail.com', sueldo: '1500' } ,
+        usuario_modelo: [{ id: 1,name: 'Mishell', email: 'Mishell@gmail.com', sueldo: '1500' } ],
         headers: [
           { text: 'Nombre', align: 'start', sortable: false, value: 'id' },
           { text: 'Nombre', align: 'start', sortable: false, value: 'name' },
           { text: 'Email',align: 'start', sortable: false, value: 'email' },
           { text: 'Sueldo',align: 'start', sortable: false, value: 'sueldo' },
         ],
-        desserts: [],// lo agrego desde el mounted, con el metodo setusertable
+        desserts:[],// lo agrego desde el mounted, con el metodo setusertable
       }
     },
     methods:{
         setUserTable(){
-            this.personas.forEach(persona => {
-                 this.desserts.push(persona);
-            });
-            // console.log('soy funcion')
+          this.getUserApi();
         },
-         async getUser () {
-           let apiRes;
-           try {
-              apiRes = await axios.get('http://localhost:3000/api/user',{
-                headers: {
-                  'x-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEiLCJlbWFpbCI6IklzYWJlbEBnbWFpbC5jb20iLCJpYXQiOjE2Mjg3MTU5NzIsImV4cCI6MTYyODcyMzE3Mn0.PScV67FpT9QB307HEGo5201cU0e4Zx5ieKUNANwV-m4'
-                }
-             })
-              this.usersApi=apiRes.data.users;
-            } catch (err) {
-              apiRes = err.response;
-            
-              Swal.fire({
-                icon: 'error',
-                title: 'Oops...',
-                text: apiRes.data.msg + ", inicie sesion e intente nuevamente",
-
-              })
-            } 
-           
-         
-      },
+       ...mapActions('usuario',['getUserApi']),
     },
     computed:{
       ...mapState('usuario',['personas']),
+      
     },
     mounted(){
       // this.desserts.push(this.usuario_modelo);
-      this.setUserTable();
-      this.getUser();
+      // this.setUserTable();
+     
     }
     
   }
